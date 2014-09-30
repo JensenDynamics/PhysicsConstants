4e4a8c0263c797860c90943728f86a1e06fbef93
ejensen141 <ejensen141@gmail.com>
2014-09-30 14:44:09 -0400

    Create PhysicsConstants.h

---
+++ PhysicsConstants.h
@@ -0,0 +1,55 @@
+#ifndef physics_const_h
+#define physics_const_h
+#include <math.h>
+
+// A = http://www.chemie.fu-berlin.de/chemistry/general/constants_en.html
+
+// define PI 
+#define _PI 3.14159265 //google
+
+//define plancks const
+#define _h 6.626068e-34  // J/s  //google
+
+//define E 
+#define _E 2.71828183    //  (math constant)
+
+//define boltzmanns constants
+#define _k_b  1.3806503e-23 // m^2 kg s-2 K-1 //google
+
+// define Speed of light
+#define _c 2.99792458e8 // m/s  from A
+
+// define Elemetary charge e
+#define _e 1.60217733e-19 // [C] from A
+
+//define Avogadros Number 
+#define _N_A 6.0221367e23 // particles/mol   from A
+
+//define Permeability of vacuum mu not
+// 4 pi e7
+#define _mu0 12.566370614e-7 // T^2·m^3/J    from A
+
+// define permittivity of vacuum  epsilon0
+#define _epsilon0 8.854187817e-12 // C2/J·m
+
+// mass of electron at rest
+#define _m_e 9.1093897e-31 // kg
+
+// mass of proton at rest
+#define _m_p 1.6726231e-27 // kg
+
+// Neutron mass at rest
+#define _m_n 1.6749286e-27 // kg
+
+// Avveleration due to gravity 
+#define _g 9.80665 // m/s2
+
+// Wiens constant  2.8977721×10−3 K m
+#define _Wiens 2.8977721e-3 // K m
+
+
+
+#endif
+
+
+
